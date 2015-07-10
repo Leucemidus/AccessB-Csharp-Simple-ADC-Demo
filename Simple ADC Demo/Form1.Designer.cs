79d66d35e50d1068b235a201873aae68e0710340
Leucemidus <dr.sodor@gmail.com>
2015-07-09 19:53:26 -0800

    Visual Studio 2012 C# AccessB ADC demo
    This is a Visual Studio 2012 project on C#, it work with an PIC18F2550
    programmed with AccessB firmware.
    
    This project show the use of ADC

---
+++ Simple ADC Demo/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Simple_ADC_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnADCconfig = new System.Windows.Forms.Button();
+            this.btnADCOn = new System.Windows.Forms.Button();
+            this.btnADCOff = new System.Windows.Forms.Button();
+            this.btnSample = new System.Windows.Forms.Button();
+            this.btnFindDev = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txbSampleVal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbChannel = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnADCconfig
+            // 
+            this.btnADCconfig.Enabled = false;
+            this.btnADCconfig.Location = new System.Drawing.Point(12, 62);
+            this.btnADCconfig.Name = "btnADCconfig";
+            this.btnADCconfig.Size = new System.Drawing.Size(134, 35);
+            this.btnADCconfig.TabIndex = 0;
+            this.btnADCconfig.Text = "Configure ADC";
+            this.btnADCconfig.UseVisualStyleBackColor = true;
+            this.btnADCconfig.Click += new System.EventHandler(this.btnADCconfig_Click);
+            // 
+            // btnADCOn
+            // 
+            this.btnADCOn.Enabled = false;
+            this.btnADCOn.Location = new System.Drawing.Point(162, 21);
+            this.btnADCOn.Name = "btnADCOn";
+            this.btnADCOn.Size = new System.Drawing.Size(134, 35);
+            this.btnADCOn.TabIndex = 0;
+            this.btnADCOn.Text = "ADC On";
+            this.btnADCOn.UseVisualStyleBackColor = true;
+            this.btnADCOn.Click += new System.EventHandler(this.btnADCOn_Click);
+            // 
+            // btnADCOff
+            // 
+            this.btnADCOff.Enabled = false;
+            this.btnADCOff.Location = new System.Drawing.Point(162, 62);
+            this.btnADCOff.Name = "btnADCOff";
+            this.btnADCOff.Size = new System.Drawing.Size(134, 35);
+            this.btnADCOff.TabIndex = 0;
+            this.btnADCOff.Text = "ADC Off";
+            this.btnADCOff.UseVisualStyleBackColor = true;
+            this.btnADCOff.Click += new System.EventHandler(this.btnADCOff_Click);
+            // 
+            // btnSample
+            // 
+            this.btnSample.Enabled = false;
+            this.btnSample.Location = new System.Drawing.Point(12, 122);
+            this.btnSample.Name = "btnSample";
+            this.btnSample.Size = new System.Drawing.Size(134, 35);
+            this.btnSample.TabIndex = 0;
+            this.btnSample.Text = "Sample";
+            this.btnSample.UseVisualStyleBackColor = true;
+            this.btnSample.Click += new System.EventHandler(this.btnSample_Click);
+            // 
+            // btnFindDev
+            // 
+            this.btnFindDev.Location = new System.Drawing.Point(12, 21);
+            this.btnFindDev.Name = "btnFindDev";
+            this.btnFindDev.Size = new System.Drawing.Size(134, 35);
+            this.btnFindDev.TabIndex = 0;
+            this.btnFindDev.Text = "Find Device";
+            this.btnFindDev.UseVisualStyleBackColor = true;
+            this.btnFindDev.Click += new System.EventHandler(this.btnFindDev_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(27, 5);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(109, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Status: Disconnected";
+            // 
+            // txbSampleVal
+            // 
+            this.txbSampleVal.Location = new System.Drawing.Point(239, 122);
+            this.txbSampleVal.Name = "txbSampleVal";
+            this.txbSampleVal.Size = new System.Drawing.Size(35, 20);
+            this.txbSampleVal.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ADC Channel:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ADC Value:";
+            // 
+            // cmbChannel
+            // 
+            this.cmbChannel.FormattingEnabled = true;
+            this.cmbChannel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.cmbChannel.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.cmbChannel.Location = new System.Drawing.Point(239, 152);
+            this.cmbChannel.Name = "cmbChannel";
+            this.cmbChannel.Size = new System.Drawing.Size(50, 21);
+            this.cmbChannel.TabIndex = 3;
+            this.cmbChannel.KeyUp += new System.Windows.Forms.KeyEventHandler(this.cmbChannelBorrar_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 189);
+            this.Controls.Add(this.cmbChannel);
+            this.Controls.Add(this.txbSampleVal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnSample);
+            this.Controls.Add(this.btnADCOff);
+            this.Controls.Add(this.btnADCOn);
+            this.Controls.Add(this.btnFindDev);
+            this.Controls.Add(this.btnADCconfig);
+            this.Name = "Form1";
+            this.Text = "Simple ADC Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnADCconfig;
+        private System.Windows.Forms.Button btnADCOn;
+        private System.Windows.Forms.Button btnADCOff;
+        private System.Windows.Forms.Button btnSample;
+        private System.Windows.Forms.Button btnFindDev;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txbSampleVal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbChannel;
+    }
+}
+
---
+++ Simple ADC Demo/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_ADC_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        AccessB AccB = new AccessB();//First declate the AccessB object
+
+        //Find the device
+        private void btnFindDev_Click(object sender, EventArgs e)
+        {
+            if (AccB.FindDevHID() == true)
+            {
+                lblStatus.Text = "Status: Connected";
+                //Enabling all the buttons
+                btnADCconfig.Enabled = true;
+                btnADCOn.Enabled = true;
+                btnSample.Enabled = true;
+                btnADCOff.Enabled = true;
+            }
+            else
+            {
+                lblStatus.Text = "Status: Disconnected";
+            }
+        }
+        
+        //Configure ADC to use all it's analog inputs AN0 to AN12, AN5,6 and 7 don't exist on PIC18F2550
+        private void btnADCconfig_Click(object sender, EventArgs e)
+        {
+            AccB.ADC_CFG(AccB.CFG_ADC_AS_Basic, AccB.ADC_AN0_To_12);
+        }
+
+        //Turn on ADC
+        private void btnADCOn_Click(object sender, EventArgs e)
+        {
+            AccB.ADC_Enable(true);
+        }
+
+        //Turn Off ADC
+        private void btnADCOff_Click(object sender, EventArgs e)
+        {
+            AccB.ADC_Enable(false);
+        }
+
+        //Obtain a sample from ADC for the selected channel in the combo box, Analog input 0 (AN0) is in the pin RA0 (pin number 2).
+        //view the pin diagrams in the 18F2550 Datasheet to know where are the other analog inputs.
+        private void btnSample_Click(object sender, EventArgs e)
+        {
+            if (cmbChannel.Text == "")
+            {
+                MessageBox.Show("Please select a valid analog input");
+                return;
+            }
+            txbSampleVal.Text = AccB.ADC_Val(Convert.ToUInt32(cmbChannel.Text)).ToString();
+        }
+
+        //If the user want to write something that isn't in the combo box list it's deleted.
+        private void cmbChannelBorrar_KeyUp(object sender, KeyEventArgs e)
+        {
+            cmbChannel.Text = "";
+        }
+
+    }
+}
